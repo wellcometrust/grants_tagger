f16c669314e4f434c1ca20c945f221046db78218
Nick Sorros <nsorros@gmail.com>
2020-09-10 11:06:30 +0000

    Fix errors

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import math
 
@@ -34,7 +35,9 @@ def create_dataset(data_path, nb_tags=DEFAULT_NB_TAGS, nb_examples_per_tag=10_00
                 texts.append(item["text"])
                 tags.append(item_tags)
             if all([c > nb_examples_per_tag for t, c in tags_count.items()]):
+                print(tags_count)
                 break
+        print(tags_count)
         return texts, tags
 
 def vectorize_data(train_texts, train_tags, test_texts, test_tags,
@@ -42,11 +45,11 @@ def vectorize_data(train_texts, train_tags, test_texts, test_tags,
                    sequence_length=DEFAULT_SEQUENCE_LENGTH):
     # fit vectorizer and transform
     vectorizer = KerasVectorizer(vocabulary_size, sequence_length)
-    X_train = vectorizer.transform(train_texts)
+    X_train = vectorizer.fit_transform(train_texts)
     X_test = vectorizer.transform(test_texts)
 
     # vectorize tags
-    label_binarizer = MultiLabelBinarizer(sparse=True)
+    label_binarizer = MultiLabelBinarizer(sparse_output=True)
     label_binarizer.fit(train_tags)
     Y_train = label_binarizer.transform(train_tags)
     Y_test = label_binarizer.transform(test_tags)
@@ -59,11 +62,11 @@ def build_model(learning_rate=0.01, batch_size=256, attention=True,
         attention=attention, multilabel=True,
         learning_rate=learning_rate, batch_size=batch_size
     )
-    model = model._build_model(vocabulary_size, sequence_length, nb_tags)
+    model = model._build_model(vocab_size=vocabulary_size, sequence_length=sequence_length, nb_outputs=nb_tags)
     return model
 
 def train(X_train, X_test, Y_train, Y_test, learning_rate=0.01, batch_size=256, nb_epochs=5):
-    logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    logdir = "logs/scalars/" + datetime.now().strftime("%Y%m%d-%H%M%S") + f"-{learning_rate}-{batch_size}"
     tensorboard = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 
     def yield_data(X, Y, batch_size, shuffle=True):
@@ -81,25 +84,33 @@ def train(X_train, X_test, Y_train, Y_test, learning_rate=0.01, batch_size=256, 
     steps_per_epoch = math.ceil(X_train.shape[0]/batch_size)
     validation_steps = math.ceil(X_test.shape[0]/batch_size)
 
-    model = build_model(learning_rate=learning_rate)
+    nb_tags = Y_train.shape[1]
+    model = build_model(learning_rate=learning_rate, nb_tags=nb_tags)
     model.fit(x=train_data, steps_per_epoch=steps_per_epoch,
               validation_data=test_data, validation_steps=validation_steps,
               epochs=nb_epochs, callbacks=[tensorboard])
 
-def learning_rate_experiment(train_data_path, test_data_path):
+def learning_rate_experiment(data_path):
     # create dataset
-    train_texts, train_tags = create_dataset(train_data_path, 32, 100)
-    test_texts, test_tags = create_dataset(test_data_path, 32, 100)
+    texts, tags = create_dataset(data_path, 32, 10)
+    print(len(texts))
+
+    # split dataset
+    nb_train = len(texts) - min(int(0.2*len(texts)), 10_000)
+    train_texts = texts[:nb_train]
+    train_tags = tags[:nb_train]
+    test_texts = texts[nb_train:]
+    test_tags = tags[nb_train:]
 
     # vectorize data
     X_train, X_test, Y_train, Y_test = vectorize_data(train_texts, train_tags, test_texts, test_tags)
 
     # for param train
-    for learning_rate in [0.1, 0.01]:
+    for learning_rate in [0.1, 0.01, 0.001, 0.0001, 0.00001]:
         # build model with params
-        train(learning_rate)
+        train(X_train, X_test, Y_train, Y_test, learning_rate, nb_epochs=50)
         # note we want to stop training when loss converges (possibly stop improving for some iterations)
 
         # evaluate on best model?
 
-learning_rate_experiment("train_data_path", "test_data_path")
+learning_rate_experiment("data/processed/disease_mesh.jsonl")

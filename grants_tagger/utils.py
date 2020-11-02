00d5d9787b6ed91f5b7883e907fb41684f20768d
Nick Sorros <nsorros@gmail.com>
2020-11-02 09:43:39 +0000

    Add test size param in train

--- grants_tagger/utils.py
+++ grants_tagger/utils.py
@@ -36,7 +36,7 @@ def load_data(data_path, label_binarizer=None, X_format="List"):
 def load_train_test_data(
         train_data_path, label_binarizer,
         test_data_path=None, from_same_distribution=False,
-        X_format="List"):
+        X_format="List", test_size=None):
 
     if test_data_path:
         X_train, Y_train, _ = load_data(train_data_path, label_binarizer, X_format)
@@ -52,7 +52,7 @@ def load_train_test_data(
     else:
         X, Y, _ = load_data(train_data_path, label_binarizer, X_format)
         X_train, X_test, Y_train, Y_test = train_test_split(
-            X, Y, random_state=42
+            X, Y, random_state=42, test_size=test_size
         )
            
     return X_train, X_test, Y_train, Y_test

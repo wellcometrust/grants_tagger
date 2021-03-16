6bb51ce85a4f843dabf329e4edf9367969ec0137
Nick Sorros <nsorros@gmail.com>
2021-03-16 13:25:34 +0200

    Use fixture in train and add tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -7,178 +7,153 @@ import os
 from sklearn.preprocessing import MultiLabelBinarizer
 from scipy.sparse import csr_matrix
 import numpy as np
+import pytest
 
 from grants_tagger.train import train_and_evaluate, create_label_binarizer
 
 
-# TODO: Use fixtures to reduce duplication
-
-def test_create_label_binarizer():
+# TODO: Use train and test_data_path fixtures
+@pytest.fixture()
+def data_path(tmp_path):
     texts = ["one", "one two", "two"]
     tags = [["one"], ["one","two"], ["two"]]
+    meta = [{}, {}, {}]
 
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        label_binarizer_path = os.path.join(tmp_dir, "label_binarizer.pkl")
-        data_path = os.path.join(tmp_dir, "data.jsonl")
+    test_data_path = os.path.join(tmp_path, "data.jsonl")
 
-        with open(data_path, "w") as f:
-            for text, tags_ in zip(texts, tags):
-                example = {"text": text, "tags": tags_}
-                f.write(json.dumps(example)+"\n")
-
-        create_label_binarizer(data_path, label_binarizer_path)
-
-        with open(label_binarizer_path, "rb") as f:
-            label_binarizer = pickle.loads(f.read())
-
-        Y = label_binarizer.transform([["one"]])
-
-        assert "one" in label_binarizer.classes_
-        assert "two" in label_binarizer.classes_
-        assert len(label_binarizer.classes_) == 2
-        assert isinstance(Y, np.ndarray)
+    with open(test_data_path, "w") as f:
+        for text, tags_, meta_ in zip(texts, tags, meta):
+            example = {"text": text, "tags": tags_, "meta": meta_}
+            f.write(json.dumps(example)+"\n")
+    return test_data_path
 
 
-def test_create_label_binarizer_sparse():
-    texts = ["one", "one two", "two"]
-    tags = [["one"], ["one","two"], ["two"]]
-
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        label_binarizer_path = os.path.join(tmp_dir, "label_binarizer.pkl")
-        data_path = os.path.join(tmp_dir, "data.jsonl")
-
-        with open(data_path, "w") as f:
-            for text, tags_ in zip(texts, tags):
-                example = {"text": text, "tags": tags_}
-                f.write(json.dumps(example)+"\n")
-
-        create_label_binarizer(data_path, label_binarizer_path, sparse=True)
-
-        with open(label_binarizer_path, "rb") as f:
-            label_binarizer = pickle.loads(f.read())
-
-        Y = label_binarizer.transform([["one"]])
-
-        assert "one" in label_binarizer.classes_
-        assert "two" in label_binarizer.classes_
-        assert len(label_binarizer.classes_) == 2
-        assert isinstance(Y, csr_matrix)
+# TODO: Use data_path fixture and create_label_binarizer
+@pytest.fixture()
+def label_binarizer_path(tmp_path):
+    label_binarizer_path = os.path.join(tmp_path, "label_binarizer.pkl")
+    
+    label_binarizer = MultiLabelBinarizer()
+    label_binarizer.fit([["one", "two"]])
+    
+    with open(label_binarizer_path, "wb") as f:
+        f.write(pickle.dumps(label_binarizer))
+    return label_binarizer_path
 
 
-def test_train_and_evaluate():
+@pytest.fixture
+def train_data_path(tmp_path):
+    texts = ["one", "one two"]
+    tags = [["one"], ["one","two"]]
+    meta = [{}, {}]
+    test_data_path = os.path.join(tmp_path, "train_data.jsonl")
+
+    with open(test_data_path, "w") as f:
+        for text, tags_, meta_ in zip(texts, tags, meta):
+            example = {"text": text, "tags": tags_, "meta": meta_}
+            f.write(json.dumps(example)+"\n")
+    return test_data_path
+
+
+@pytest.fixture
+def test_data_path(tmp_path):
+    texts = ["two"]
+    tags = [["two"]]
+    meta = [{}]
+
+    test_data_path = os.path.join(tmp_path, "test_data.jsonl")
+
+    with open(test_data_path, "w") as f:
+        for text, tags_, meta_ in zip(texts, tags, meta):
+            example = {"text": text, "tags": tags_, "meta": meta_}
+            f.write(json.dumps(example)+"\n")
+    return test_data_path
+
+
+def test_create_label_binarizer(tmp_path, data_path):
+    label_binarizer_path = os.path.join(tmp_path, "label_binarizer.pkl")
+    create_label_binarizer(data_path, label_binarizer_path)
+
+    with open(label_binarizer_path, "rb") as f:
+        label_binarizer = pickle.loads(f.read())
+
+    Y = label_binarizer.transform([["one"]])
+
+    assert "one" in label_binarizer.classes_
+    assert "two" in label_binarizer.classes_
+    assert len(label_binarizer.classes_) == 2
+    assert isinstance(Y, np.ndarray)
+
+
+def test_create_label_binarizer_sparse(tmp_path, data_path):
+    label_binarizer_path = os.path.join(tmp_path, "label_binarizer.pkl")
+
+    create_label_binarizer(data_path, label_binarizer_path, sparse=True)
+
+    with open(label_binarizer_path, "rb") as f:
+        label_binarizer = pickle.loads(f.read())
+
+    Y = label_binarizer.transform([["one"]])
+
+    assert "one" in label_binarizer.classes_
+    assert "two" in label_binarizer.classes_
+    assert len(label_binarizer.classes_) == 2
+    assert isinstance(Y, csr_matrix)
+
+
+def test_train_and_evaluate(data_path, label_binarizer_path):
     approach = "tfidf-svm"
 
-    texts = ["one", "one two", "two"]
-    tags = [["one"], ["one", "two"], ["two"]]
-    with tempfile.NamedTemporaryFile("r+") as train_data_tmp:
-        label_binarizer = MultiLabelBinarizer()
-        label_binarizer.fit(tags)
-        with tempfile.NamedTemporaryFile() as label_binarizer_tmp:
-            label_binarizer_tmp.write(pickle.dumps(label_binarizer))
-
-            for text, tags_ in zip(texts, tags):
-                train_data_tmp.write(json.dumps({"text": text, "tags": tags_, "meta": {}}))
-                train_data_tmp.write("\n")
-
-            train_data_tmp.seek(0)
-            label_binarizer_tmp.seek(0)
-
-            train_and_evaluate(train_data_tmp.name, label_binarizer_tmp.name, approach,
-                               parameters="{'tfidf__min_df': 1, 'tfidf__stop_words': None}")
+    train_and_evaluate(data_path, label_binarizer_path, approach,
+        parameters="{'tfidf__min_df': 1, 'tfidf__stop_words': None}")
 
 
-def test_train_pickle_save():
-    pass
+def test_train_pickle_save(tmp_path, data_path, label_binarizer_path):
+    approach = "tfidf-svm"
 
-def test_train_model_save():
+    model_path = os.path.join(tmp_path, "model.pkl")
+    train_and_evaluate(data_path, label_binarizer_path, approach=approach,
+        model_path=model_path, parameters="{'tfidf__min_df': 1, 'tfidf__stop_words': None}")
+    assert os.path.exists(model_path)
+
+
+def test_train_model_save(tmp_path, data_path, label_binarizer_path):
     approach = "mesh-cnn"
 
-    texts = ["one", "one two", "two"]
-    tags = [["one"], ["one","two"], ["two"]]
+    train_and_evaluate(data_path, label_binarizer_path,
+        approach, model_path=tmp_path)
 
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        label_binarizer_path = os.path.join(tmp_dir, "label_binarizer.pkl")
-        train_data_path = os.path.join(tmp_dir, "train_data.jsonl")
-
-        label_binarizer = MultiLabelBinarizer()
-        label_binarizer.fit(tags)
-        with open(label_binarizer_path, "wb") as label_binarizer_tmp:
-            label_binarizer_tmp.write(pickle.dumps(label_binarizer))
-
-        with open(train_data_path, "w") as train_data_tmp:
-            for text, tags_ in zip(texts, tags):
-                train_data_tmp.write(json.dumps({"text": text, "tags": tags_, "meta": {}}))
-                train_data_tmp.write("\n")
-
-        train_and_evaluate(train_data_tmp.name, label_binarizer_tmp.name,
-                           approach, model_path=tmp_dir)
-
-        expected_vectorizer_path = os.path.join(tmp_dir, "vectorizer.pkl")
-        expected_model_variables_path = os.path.join(tmp_dir, "variables")
-        expected_model_assets_path = os.path.join(tmp_dir, "assets")
-        assert os.path.exists(expected_vectorizer_path)
-        assert os.path.exists(expected_model_variables_path)
-        assert os.path.exists(expected_model_assets_path)
+    expected_vectorizer_path = os.path.join(tmp_path, "vectorizer.pkl")
+    expected_model_variables_path = os.path.join(tmp_path, "variables")
+    expected_model_assets_path = os.path.join(tmp_path, "assets")
+    assert os.path.exists(expected_vectorizer_path)
+    assert os.path.exists(expected_model_variables_path)
+    assert os.path.exists(expected_model_assets_path)
 
 
-def test_train_and_evaluate_generator():
+def test_train_and_evaluate_generator(train_data_path, test_data_path, label_binarizer_path):
     approach = "mesh-cnn"
 
-    train_texts = ["one", "one two"]
-    train_tags = [["one"], ["one", "two"]]
-    
-    test_texts = ["two"]
-    test_tags = [["two"]]
-
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        train_data_path = os.path.join(tmp_dir, "train_data.jsonl")
-        test_data_path = os.path.join(tmp_dir, "test_data.jsonl")
-        label_binarizer_path = os.path.join(tmp_dir, "label_binarizer.pkl")
-
-        with open(train_data_path, "w") as f:
-            for text, tags_ in zip(train_texts, train_tags):
-                f.write(json.dumps({"text": text, "tags": tags_, "meta": {}}))
-                f.write("\n")
-
-        with open(test_data_path, "w") as f:
-            for text, tags_ in zip(test_texts, test_tags):
-                f.write(json.dumps({"text": text, "tags": tags_, "meta": {}}))
-                f.write("\n")
-
-        train_and_evaluate(train_data_path, label_binarizer_path, approach,
-                           data_format="generator", sparse_labels=True, test_data_path=test_data_path)
+    train_and_evaluate(train_data_path, label_binarizer_path, approach,
+        data_format="generator", sparse_labels=True, test_data_path=test_data_path)
 
 
-def test_train_and_evaluate_generator_non_sparse_labels():
+def test_train_and_evaluate_generator_non_sparse_labels(train_data_path, test_data_path, label_binarizer_path):
     approach = "mesh-cnn"
 
-    train_texts = ["one", "one two"]
-    train_tags = [["one"], ["one", "two"]]
-    
-    test_texts = ["two"]
-    test_tags = [["two"]]
-
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        train_data_path = os.path.join(tmp_dir, "train_data.jsonl")
-        test_data_path = os.path.join(tmp_dir, "test_data.jsonl")
-        label_binarizer_path = os.path.join(tmp_dir, "label_binarizer.pkl")
-
-        with open(train_data_path, "w") as f:
-            for text, tags_ in zip(train_texts, train_tags):
-                f.write(json.dumps({"text": text, "tags": tags_, "meta": {}}))
-                f.write("\n")
-
-        with open(test_data_path, "w") as f:
-            for text, tags_ in zip(test_texts, test_tags):
-                f.write(json.dumps({"text": text, "tags": tags_, "meta": {}}))
-                f.write("\n")
-        
-        train_and_evaluate(train_data_path, label_binarizer_path, approach,
-                           data_format="generator", test_data_path=test_data_path)
+    train_and_evaluate(train_data_path, label_binarizer_path, approach,
+        data_format="generator", test_data_path=test_data_path)
 
 
-def test_train_and_evaluate_threshold():
-    pass
+def test_train_and_evaluate_threshold(data_path, label_binarizer_path):
+    approach = "tfidf-svm"
+
+    f1 = train_and_evaluate(data_path, label_binarizer_path, approach=approach,
+        threshold=1, parameters="{'tfidf__min_df': 1, 'tfidf__stop_words': None}")
+    assert f1 == 0
+    f1 = train_and_evaluate(data_path, label_binarizer_path, approach=approach,
+        threshold=0, parameters="{'tfidf__min_df': 1, 'tfidf__stop_words': None}")
+    assert f1 == (2/3) # P: 0.5, R: 1.0, F: 0.66
 
 
 # TODO: Move to models

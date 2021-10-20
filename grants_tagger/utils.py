24446b8d98eee7c8aac7e732e7e2b4663c228730
aCampello <a.campello@wellcome.ac.uk>
2021-10-20 16:29:52 +0100

    Simplify the instance type

--- grants_tagger/utils.py
+++ grants_tagger/utils.py
@@ -3,8 +3,7 @@ from functools import partial
 import pickle
 import json
 
-import boto3
-import botocore
+import requests
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 import tensorflow as tf
@@ -103,19 +102,10 @@ def calc_performance_per_tag(Y_true, Y_pred, tags):
 def get_ec2_instance_type():
     """Utility function to get ec2 instance name, or empty string if not possible to get name"""
 
-    instance_id_request = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
+    instance_type_request = requests.get('http://169.254.169.254/latest/meta-data/instance-type')
 
-    instance_type = ""
-
-    if instance_id_request == 200:
-        ec2 = boto3.resource('ec2')
-        ec2instance = ec2.Instance(instance_id_request.content)
-
-        try:
-            instance_type = ec2instance.instance_type
-        except botocore.exceptions.ClientError:
-            # Do nothing here, it will return an empty string instance_type in this case
-            pass
-
-    return instance_type
+    if instance_type_request.status_code == 200:
+        return instance_type.content.decode()
+    else:
+        return ""
 

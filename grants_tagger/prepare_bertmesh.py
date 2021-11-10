0c36fdb47a1100322798fed5a0e860bba772fa5b
Nick Sorros <nsorros@gmail.com>
2021-11-10 17:56:37 +0000

    WIP bertmesh

--- grants_tagger/prepare_bertmesh.py
+++ grants_tagger/prepare_bertmesh.py
@@ -2,7 +2,7 @@ import pickle
 import os
 
 from sklearn.preprocessing import MultiLabelBinarizer
-from transformers import BertTokenizer
+from transformers import BertTokenizerFast
 import scipy.sparse as sp
 import numpy as np
 import typer
@@ -17,8 +17,17 @@ def save_pickle(obj_path, obj):
     with open(obj_path, "wb") as f:
         f.write(pickle.dumps(obj))
 
-def prepare_bertmesh(data_path, x_path, y_path, label_binarizer_path, tokenizer_path=None):
-    X, Y, _ = load_data(data_path)
+def prepare_bertmesh(data_path, x_path, y_path, label_binarizer_path, tokenizer_path=None, years=None):
+    X, Y, meta = load_data(data_path)
+
+    if years:
+        print("filtering data")
+        min_year, max_year = [int(year) for year in years.split(",")]
+        print("   calculating sample size")
+        sample_indices = [i for i, m in enumerate(meta) if m["year"] and (min_year <= int(m["year"]) <= max_year)]
+        print(f"   sample_size {len(sample_indices)}")
+        X = [X[i] for i in sample_indices]
+        Y = [Y[i] for i in sample_indices]
 
     if os.path.exists(label_binarizer_path):
         print("loading existing")
@@ -30,9 +39,12 @@ def prepare_bertmesh(data_path, x_path, y_path, label_binarizer_path, tokenizer_
     
     print("transforming data")
     Y_vec = label_binarizer.transform(Y)
+    print(f"   number of labels {Y_vec.shape[1]}")
 
-    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
     X_vec = tokenizer(X, truncation=True, padding=True)["input_ids"]
+    X_vec = np.array(X_vec)
+    print(f"   number of features {X_vec.shape[1]}")
 
     print("saving")
     np.save(x_path, X_vec)
--- grants_tagger/train_bertmesh.py
+++ grants_tagger/train_bertmesh.py
@@ -29,12 +29,13 @@ class MultiLabelAttention(tf.keras.layers.Layer):
         # inputs (batch_size, seq_len, emb_dim)
         return tf.matmul(attention_weights, inputs, tranpose_a=True) # (batch_size, labels, emb_dim)
 
-def train_bertmesh(x_path, y_path, model_path, multilabel_attention=False):
+def train_bertmesh(x_path, y_path, model_path, multilabel_attention=False,
+        batch_size: int=256, learning_rate: float =1e-4, epochs: int=5, pretrained_model="bert-base-uncased"):
     X = np.load(x_path)
     Y = sp.load_npz(y_path)
 
     inputs = tf.keras.Input(shape=X.shape[1], dtype=X.dtype)
-    bert = TFBertModel.from_pretrained("bert-base-uncased")
+    bert = TFBertModel.from_pretrained(pretrained_model)
 
     # TODO: Optionally concatenate vectors from 4 last layers
     cls = bert(inputs)[0]
@@ -50,28 +51,28 @@ def train_bertmesh(x_path, y_path, model_path, multilabel_attention=False):
     model = tf.keras.Model(inputs=inputs, outputs=out)
     print(model.summary())
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     metrics = [tf.keras.metrics.Precision(name="precision"), tf.keras.metrics.Recall(name="recall"),
         tfa.metrics.F1Score(Y.shape[1], average="micro", threshold=0.5, name="f1")]
     model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=metrics)
     
-    def yield_data(X, Y, batch_size):
+    def yield_data(X, Y, batch_size, shuffle=True):
         indices = list(range(X.shape[0]))
         while True:
-            random.shuffle(indices)
-            X = X[indices,:]
-            Y = Y[indices,:]
+            if shuffle:
+                random.shuffle(indices)
+                X = X[indices,:]
+                Y = Y[indices,:]
             for i in range(0, X.shape[0], batch_size):
                 yield X[i:i+batch_size,:], Y[i:i+batch_size,:].todense()
     
-    batch_size = 256
     steps_per_epoch = math.ceil(X.shape[0]/batch_size)
     
-    train_data = yield_data(X, Y, batch_size)
-    print("iterate over data")
-    batches = sum([1 for _ in train_data])
-    print(f"found {batches} batches")
-    model.fit(train_data, epochs=5, steps_per_epoch=steps_per_epoch)
+    train_data = yield_data(X, Y, batch_size, shuffle=False)
+    #print("iterate over data")
+    #batches = sum([1 for _ in train_data])
+    #print(f"found {batches} batches")
+    model.fit(train_data, epochs=epochs, steps_per_epoch=steps_per_epoch)
     model.save(model_path)
 
 

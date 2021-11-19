4d4bb3e50e7d77336da79109f643513c9c261f32
aCampello <a.campello@wellcome.ac.uk>
2021-11-19 12:38:58 +0000

    Add get grants

---
+++ scripts/get_grants.py
@@ -0,0 +1,24 @@
+#!/bin/bash
+import pandas as pd
+
+import os
+
+from datascience.warehouse.warehouse import FortyTwo
+
+forty_two = FortyTwo()
+df = pd.DataFrame(forty_two.get_grants())
+
+columns_of_interest = [
+    'Cost Centre Division Name',
+    'Master Grant Type Name',
+    'Reference',
+    'Title',
+    'Synopsis',
+    'Lay Summary',
+    'Research Question',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+df[columns_of_interest].to_csv(os.path.join(here, '../data/raw/grants.csv'))
+
+print(f"Downloaded {len(df)} grants")

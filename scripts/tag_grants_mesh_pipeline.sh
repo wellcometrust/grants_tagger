62e16a85500e3528dc1efc645bed61147f35862b
aCampello <a.campello@wellcome.ac.uk>
2021-11-19 16:36:00 +0000

    Remove accidental pipeline file

--- scripts/tag_grants_mesh_pipeline.sh
+++
@@ -1,7 +0,0 @@
-python /pipelines/mesh/task_tagger.py \ 
-	--input ../data/raw/grants.csv \
-	--output temp.csv \
-	--model_path models/xlinear/model \
-	--label_binarizer_path models/xlinear/label_binarizer.pkl \
-        --cap 100
-          

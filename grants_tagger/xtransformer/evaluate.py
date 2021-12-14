4e0a89a6c719978e90560543c91dc2b501ed921e
Nick Sorros <nsorros@gmail.com>
2021-12-14 17:26:47 +0200

    Add pr curve

--- grants_tagger/xtransformer/evaluate.py
+++ grants_tagger/xtransformer/evaluate.py
@@ -5,20 +5,23 @@ import scipy.sparse as sp
 import typer
 
 
-def evaluate(y_test_path, y_pred_path, results_path):
+def evaluate(y_test_path, y_pred_path, results_path, pr_curve_path):
     Y_test = sp.load_npz(y_test_path)
     Y_pred_proba = sp.load_npz(y_pred_path)
 
+    pr_curve = []
     for th in [0.001, 0.005, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5]:
         Y_pred = Y_pred_proba > th
         p, r, f1, _ = precision_recall_fscore_support(Y_test, Y_pred, average="micro")
+        pr_curve.append({"precision": p, "recall": r, "f1": f1, "threshold": threshold})
         print(f"Th: {th} P: {p} R: {r} f1: {f1}")
 
-    Y_pred = Y_pred_proba > 0.5
-    p, r, f1, _ = precision_recall_fscore_support(Y_test, Y_pred, average="micro")
+    with open(pr_curve_path, "w") as f:
+        f.write(json.dumps({"pr_curve": pr_curve}))
 
-    with open(results_path) as f:
-        f.write(json.dumps({"precision": p, "recall": r, "f1": f1}))
+    best_metrics = max(pr_curve, key=lambda x: x["f1"])
+    with open(results_path, "w") as f:
+        f.write(json.dumps(best_metrics))
 
 
 if __name__ == "__main__":

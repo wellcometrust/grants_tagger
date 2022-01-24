865deece8b7ce7f021306a8ec13df53a46f6fab6
Nick Sorros <nsorros@gmail.com>
2022-01-24 15:54:29 +0200

    Fix submit

--- scripts/submit_bioasq_test_results.py
+++ scripts/submit_bioasq_test_results.py
@@ -4,18 +4,19 @@ import json
 
 
 def submit_test_results(test_number, tagged_data_path, system, username, password):
-    tagged_data = json.load(tagged_data_path)
+    with open(tagged_data_path) as f:
+        tagged_data = json.load(f)
 
     url = f"http://participants-area.bioasq.org/tests/uploadResults/{test_number}/"
 
     data = {
         "username": username,
         "password": password,
-        "documents": tagged_data["tagged_data"]
+        "documents": tagged_data["tagged_data"],
         "system": system
     }
     response = requests.post(url, data=json.dumps(data))
-    print(f"Response code {reponse.status_code}")
+    print(f"Response code {response.status_code}")
 
 if __name__ == "__main__":
     typer.run(submit_test_results)

556973013578f44be7ea986bce105429706f98b6
aCampello <antonio.campello@icloud.com>
2022-08-22 12:28:04 -0300

    clean up setup

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup, find_packages
-import os
 
 from grants_tagger.__version__ import __version__
-
 
 setup(
     name="grants-tagger",
@@ -12,21 +10,25 @@ setup(
     packages=find_packages(),
     version=__version__,
     entry_points={"console_scripts": "grants_tagger=grants_tagger.__main__:app"},
-    install_requires=[
-        "pandas",
-        "xlrd",
-        "scikit-learn==0.23.2",
-        "matplotlib",
-        "wellcomeml[tensorflow,core,transformers,torch]==2.0.3",
-        "docutils==0.15",
-        "scipy==1.4.1",
-        "wasabi",
-        "typer",
-        "tqdm",
-        "streamlit",
-        "requests",
-        "pygtrie==2.3.3",
-        "openpyxl",
-    ],
+    install_requires=["sklearn", "pandas", "numpy", "scipy", "requests"],
+    extras_require={
+        "xlinear": ["libpecos==0.1.0"],
+        "dev": [
+            "pandas",
+            "xlrd",
+            "scikit-learn==0.23.2",
+            "matplotlib",
+            "wellcomeml[tensorflow,core,transformers,torch]==2.0.3",
+            "docutils==0.15",
+            "scipy==1.4.1",
+            "wasabi",
+            "typer",
+            "tqdm",
+            "streamlit",
+            "requests",
+            "pygtrie==2.3.3",
+            "openpyxl",
+        ],
+    },
     tests_require=["pytest", "tox"],
 )

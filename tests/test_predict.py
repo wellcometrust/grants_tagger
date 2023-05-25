9defbf247003decba123920e53a1b89ecca70302
Andrei Apostol <andrei.apostol@omnimodular.com>
2023-05-25 11:48:19 +0300

    add test for prediction function

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,35 +1,10 @@
-# from unittest.mock import patch
-# import tempfile
-# import shutil
-# import pickle
-# import json
-# import os
-
-# from transformers import AutoTokenizer, AutoConfig
-# import numpy as np
-# import pytest
-# import torch
-
-# from grants_tagger.train import create_label_binarizer, train
-# from grants_tagger.predict import predict_tags
-# from grants_tagger.bertmesh.model import BertMesh
-
-# try:
-#     from grants_tagger.models.mesh_xlinear import MeshXLinear
-
-#     MESH_XLINEAR_IMPORTED = True
-# except ImportError:
-#     MESH_XLINEAR_IMPORTED = False
-
-# X = ["all", "one two", "two", "four", "twenty four"]
-
-# Y = [[str(i) for i in range(24)], ["1", "2"], ["2"], ["4"], ["23"]]
-
-# Y_mesh = [[str(i) for i in range(5000)], ["1", "2"], ["2"], ["200"], ["1000"]]
+from grants_tagger.predict import predict_tags
 
 
-# def create_data(X, Y, data_path):
-#     with open(data_path, "w") as f:
-#         for x, y in zip(X, Y):
-#             f.write(json.dumps({"text": x, "tags": y, "meta": {}}))
-#             f.write("\n")
+def test_predict_tags():
+    tags = predict_tags("This is a grant about malaria", "Wellcome/WellcomeBertMesh")
+    tags = tags[0]
+
+    assert "Malaria" in tags
+    assert "Neoplasms" not in tags
+    assert len(tags) < 10

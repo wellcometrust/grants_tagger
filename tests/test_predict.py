0e7722343c7b4bf8c77901298b3a94a4c307219f
Andrei Apostol <andrei.apostol@omnimodular.com>
2023-06-01 13:44:15 +0300

    add test for batch prediction

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -8,3 +8,17 @@ def test_predict_tags():
     assert "Malaria" in tags
     assert "Neoplasms" not in tags
     assert len(tags) < 10
+
+
+def test_predict_tags_batch():
+    tags = predict_tags(
+        [
+            "This is a grant about malaria",
+            "This is a grant about HIV",
+            "This is a grant about autism",
+        ],
+        "Wellcome/WellcomeBertMesh",
+    )
+
+    assert len(tags) == 3
+    assert len(tags[0]) < 10

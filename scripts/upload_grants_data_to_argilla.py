c1423a9b6f7482b9306091adf1672281485dc9f9
Andrei Apostol <andrei.apostol@omnimodular.com>
2023-06-01 15:13:31 +0300

    add docstring

--- scripts/upload_grants_data_to_argilla.py
+++ scripts/upload_grants_data_to_argilla.py
@@ -5,6 +5,23 @@ import json
 
 
 def upload_data_to_argilla(path: str):
+    """
+    Uploads data to Argilla. The data should be in jsonl format, with each line containing a json object with the
+    following fields:
+        - abstract (str): The abstract of the grant.
+        - mesh_terms (list): A list of MeSH terms associated with the grant.
+    To generate this data, use the create_grants_sample.py script.
+
+    Args:
+        path (str): The path to the jsonl file.
+
+    Returns:
+        None
+
+    Environment Variables:
+        ARGILLA_API_URL (str): The URL of the Argilla API. Defaults to "https://pro.argilla.io".
+        ARGILLA_API_KEY (str): The API key for the Argilla API.
+    """
     api_url = os.environ.get("ARGILLA_API_URL", "https://pro.argilla.io")
     api_key = os.environ.get("ARGILLA_API_KEY")
     rg.init(
